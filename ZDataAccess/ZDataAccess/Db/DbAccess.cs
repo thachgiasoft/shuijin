2841feb20f5002854884947abfea29272fe8957a
yunchen1008 <564857808@qq.com>
2018-09-11 21:37:31 +0800

    添加各种数据库的访问对象

--- ZDataAccess/ZDataAccess/Db/DbAccess.cs
+++ ZDataAccess/ZDataAccess/Db/DbAccess.cs
@@ -4,13 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ZCSharpLib.ZTObject;
+using ZDataAccess.Db.DbSort;
 
 namespace ZDataAccess.Db
 {
     public enum DbType
     {
-        None,
+        None = 0,
         MySQL,
+        SQLServer,
+        SQLLite,
+        Orcale,
+        Odbc,
+        Max
     }
 
     public class DbAccess
@@ -22,8 +28,37 @@ namespace ZDataAccess.Db
             AccesserPool = new ZObjectPool<DbAccesser>(num);
             for (int i = 0; i < AccesserPool.Count; i++)
             {
-
+                DbAccesser dbAccesser = Create(dbType);
+                if (dbAccesser != null) AccesserPool.Push(dbAccesser);
             }
+        }
+
+
+
+        private DbAccesser Create(DbType dbType)
+        {
+            DbAccesser dbAccesser = null;
+            if (dbType == DbType.MySQL)
+            {
+                dbAccesser = new MySqlAccesser();
+            }
+            else if (dbType == DbType.SQLServer)
+            {
+                dbAccesser = new SqlServerAccesser();
+            }
+            else if (dbType == DbType.SQLLite)
+            {
+                dbAccesser = new SqlLiteAccesser();
+            }
+            else if (dbType == DbType.Orcale)
+            {
+                dbAccesser = new OrcaleAccesser();
+            }
+            else if (dbType == DbType.Odbc)
+            {
+                dbAccesser = new OdbcAccesser();
+            }
+            return dbAccesser;
         }
     }
 }
--- ZDataAccess/ZDataAccess/Db/DbAccesser.cs
+++ ZDataAccess/ZDataAccess/Db/DbAccesser.cs
@@ -8,8 +8,12 @@ namespace ZDataAccess.Db
 {
     public abstract class DbAccesser
     {
-        public DbAccesser()
-        {
-        }
+        public string Server { get; set; }
+        public string Database { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string IntergratedSecurity { get; set; }
+
+        public DbAccesser() { }
     }
 }
