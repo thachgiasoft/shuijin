f263fc15b8d98c3c07ca8d0f0175d6f36c96f1dd
yunchen1008 <564857808@qq.com>
2018-09-11 09:36:48 +0800

    modify DbAccess

--- ZDataAccess/ZDataAccess/Db/DbAccess.cs
+++ ZDataAccess/ZDataAccess/Db/DbAccess.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZCSharpLib.ZTObject;
 
 namespace ZDataAccess.Db
 {
     public class DbAccess
     {
+        private ZObjectPool<DbAccesser> AccesserPool;
+
+        public DbAccess(int num)
+        {
+            AccesserPool = new ZObjectPool<DbAccesser>(num);
+        }
     }
 }
